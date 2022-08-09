6d51d6c37e5f900a72b3a036e6b1217eb39163a2
MdMuzahid07 <MdMuzahid7396@gamil.com>
2022-08-10 03:13:41 +0600

    Todo Application with CRUD operations, is complete

--- src/components/UpdateModal.jsx
+++ src/components/UpdateModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 
 
 const UpdateModal = ({ idForUpdate }) => {
@@ -25,17 +25,12 @@ const UpdateModal = ({ idForUpdate }) => {
         })
             .then(response => response.json())
             .then(result => {
-                console.log(result)
+                if (result.modifiedCount === 1) {
+                    toast.success(`Id: ${taskId} Update successfully!`, {
+                        position: toast.POSITION.TOP_CENTER
+                    });
+                }
             })
-
-        console.log(updatedTask)
-
-        // e.reset()
-        // toast.success("Update task successfully!", {
-        //     position: toast.POSITION.TOP_CENTER
-        // });
-
-
     }
 
     return (
@@ -48,7 +43,7 @@ const UpdateModal = ({ idForUpdate }) => {
                         <input type="text" name="title" placeholder="title" className="input input-bordered w-full" required />
                         <input type="text" name="task" placeholder="task" className="input input-bordered w-full my-2" required />
                         <div class="modal-action">
-                            <button type="submit" href="#" className="btn btn-circle btn-outline"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <button type="submit" href="#" className="btn btn-circle btn-outline"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15" />
                             </svg></button>
                         </div>
